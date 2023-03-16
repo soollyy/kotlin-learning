55842ac82990d0ee700a049faed116bf342873d3
soollyy <solianatesema1@gmail.com>
2023-03-16 11:16:04 +0000

    pushing all work

---
+++ src/main/kotlin/draft.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Please type your name and hit Enter.")
+    // Write your code below
+    var myName = readLine()
+
+    println("Your name is $myName!")
+}
+//doesn't work, figure out how to work it
