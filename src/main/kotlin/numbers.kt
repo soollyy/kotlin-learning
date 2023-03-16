652abbe9994dee8be582d23a23dc79fa3341c44b
soollyy <solianatesema1@gmail.com>
2023-03-16 11:20:09 +0000

    numbers

---
+++ src/main/kotlin/numbers.kt
@@ -0,0 +1,14 @@
+//fun main() {
+//
+//    // Appetizer
+//    val caesarSalad = 8.50
+//
+//    //  Entree
+//    val beefStroganoff = 18
+//
+//    // Dessert
+//    val cheesecake = 6
+//
+//    // Add your code below ⬇️
+//    println(18 * 6)
+//}
