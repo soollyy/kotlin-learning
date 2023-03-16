1e7f923b5220c187fbb5f4eaa78182882b00ed7a
soollyy <solianatesema1@gmail.com>
2023-03-16 10:45:05 +0000

    updated files, working and running now for basic kotlin learning

---
+++ src/main/kotlin/array.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+    println(cars[0][1])
+}
+/* println(cars[0][1]) is showing the value of [0] which is 'volvo' and [1] is showing the character in 'volvo'
+at space 1 so the character 'o' */
