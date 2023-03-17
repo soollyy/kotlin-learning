bd0a1083df1389d6c584e19db302a211a00913c9
soollyy <solianatesema1@gmail.com>
2023-03-17 10:57:44 +0000

    added more work for expressions_statements_arguments

---
+++ src/main/kotlin/expressions_statements_arguments.kt
@@ -0,0 +1,13 @@
+//expressions have value
+//statements do not have value
+
+//eg of expression: 3+6 because it has a value of 9
+//also 4.7 * 11.8 is an expression
+
+// eg of statement: val greeting = "hey there" because statements can also be assignments
+
+fun main() {
+    val runnable = 4.7 * 11.8
+    println(runnable)
+}
+
